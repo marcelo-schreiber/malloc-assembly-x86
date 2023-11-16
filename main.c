e9d37f27fe9278a9ef828a3ca5fc571e32ebfa06
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-11-16 18:50:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,69 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "memalloc.h"
+#include "memalloc.h" 							
 
-extern void *brk_atual; // Você precisa ter a variável global que armazena o valor de brk como um extern aqui.
-                          // No código de teste estou chamandando de , mas se utilizarem outro nome,
-                          // substituir as ocorrências por ele aqui.
+extern void* brk_inicial;
+extern void* brk_atual;
 
-void double_alloc_after_free(void **pointers_array)
-{
-    pointers_array[1] = memory_alloc(50);
-    pointers_array[2] = memory_alloc(100);
-}
+int main() { 
 
-void simple_alloc(void **pointers_array)
-{
-    pointers_array[0] = memory_alloc(100);
-}
+	printf("============================== ROTINAS DE TESTE ==============================\n");
+	
+	setup_brk();
+	void *initial_brk = brk_inicial;
+	void *f_pnt, *s_pnt, *t_pnt;
+	
+	f_pnt = memory_alloc(100);
+	
+	printf("==>> ALOCANDO UM ESPAÇO DE 100 BYTES:\n");
+	printf("\tLOCAL: %s\n", f_pnt-16 == initial_brk ? "CORRETO!" : "INCORRETO!");
+	printf("\tIND. DE USO: %s\n", *((long long*) (f_pnt-16)) == 1 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (f_pnt-8)) == 100 ? "CORRETO!" : "INCORRETO!");
 
-int main()
-{
+	
 
-    printf("============================== ROTINAS DE TESTE ==============================\n");
 
-    setup_brk();
-    void *alloc_pointers[3];
+	printf("==>> DESALOCANDO UM ESPAÇO DE 100 BYTES:\n");
+	memory_free(f_pnt);
+	printf("\tIND. DE USO: %s\n", *((long long*) (f_pnt-16)) == 0 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (f_pnt-8)) == 100 ? "CORRETO!" : "INCORRETO!");
 
-    simple_alloc(alloc_pointers);
-    printf("==>> ALOCANDO UM ESPAÇO DE 100 BYTES:\n");
-    printf("\tLOCAL: %s\n", alloc_pointers[0] - 16 == brk_atual ? "CORRETO!" : "INCORRETO!");
-    printf("\tIND. DE USO: %s\n", *((long long *)(alloc_pointers[0] - 16)) == 1 ? "CORRETO!" : "INCORRETO!");
-    printf("\tTAMANHO: %s\n", *((long long *)(alloc_pointers[0] - 8)) == 100 ? "CORRETO!" : "INCORRETO!");
+	
+	s_pnt = memory_alloc(50);
 
-    printf("==>> DESALOCANDO UM ESPAÇO DE 100 BYTES:\n");
-    memory_free(alloc_pointers[0]);
-    printf("\tIND. DE USO: %s\n", *((long long *)(alloc_pointers[0] - 16)) == 0 ? "CORRETO!" : "INCORRETO!");
-    printf("\tTAMANHO: %s\n", *((long long *)(alloc_pointers[0] - 8)) == 100 ? "CORRETO!" : "INCORRETO!");
+	/*
+	
+	t_pnt = memory_alloc(100);
 
-    double_alloc_after_free(alloc_pointers);
-    printf("==>> ALOCANDO UM ESPAÇO DE 50 BYTES:\n");
-    printf("\tLOCAL: %s\n", alloc_pointers[1] - 16 == brk_atual ? "CORRETO!" : "INCORRETO!");
-    printf("\tIND. DE USO: %s\n", *((long long *)(alloc_pointers[1] - 16)) == 1 ? "CORRETO!" : "INCORRETO!");
-    printf("\tTAMANHO: %s\n", *((long long *)(alloc_pointers[1] - 8)) == 50 ? "CORRETO!" : "INCORRETO!");
 
-    printf("==>> ALOCANDO UM ESPAÇO DE 100 BYTES:\n");
-    printf("\tLOCAL: %s\n", alloc_pointers[1] + 100 == alloc_pointers[2] - 16 ? "CORRETO!" : "INCORRETO!");
-    printf("\tIND. DE USO: %s\n", *((long long *)(alloc_pointers[2] - 16)) == 1 ? "CORRETO!" : "INCORRETO!");
-    printf("\tTAMANHO: %s\n", *((long long *)(alloc_pointers[2] - 8)) == 100 ? "CORRETO!" : "INCORRETO!");
+	printf("==>> ALOCANDO UM ESPAÇO DE 50 BYTES:\n");
+	printf("\tLOCAL: %s\n", s_pnt-16 == initial_brk ? "CORRETO!" : "INCORRETO!");
+	printf("\tIND. DE USO: %s\n", *((long long*) (s_pnt-16)) == 1 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (s_pnt-8)) == 50 ? "CORRETO!" : "INCORRETO!");
+	printf("==>> ALOCANDO UM ESPAÇO DE 100 BYTES:\n");
+	printf("\tLOCAL: %s\n", s_pnt+100 == t_pnt-16 ? "CORRETO!" : "INCORRETO!");
+	printf("\tIND. DE USO: %s\n", *((long long*) (t_pnt-16)) == 1 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (t_pnt-8)) == 100 ? "CORRETO!" : "INCORRETO!");
+	printf("==> VERIFICANDO A FRAGMENTAÇÃO DE MEMÓRIA:\n");
+	printf("\tIND. DE USO: %s\n", *((long long*) (s_pnt+50)) == 0 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (s_pnt+58)) == 34 ? "CORRETO!" : "INCORRETO!");
 
-    printf("==> VERIFICANDO A FRAGMENTAÇÃO DE MEMÓRIA:\n");
-    printf("\tIND. DE USO: %s\n", *((long long *)(alloc_pointers[1] + 50)) == 0 ? "CORRETO!" : "INCORRETO!");
-    printf("\tTAMANHO: %s\n", *((long long *)(alloc_pointers[1] + 58)) == 34 ? "CORRETO!" : "INCORRETO!");
+	printf("==>> DESALOCANDO TUDO:\n");
+	memory_free(s_pnt);
+	memory_free(t_pnt);
+	printf("\tIND. DE USO: %s\n", *((long long*) (s_pnt-16)) == 0 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (s_pnt-8)) == 50 ? "CORRETO!" : "INCORRETO!");
+	printf("\tIND. DE USO: %s\n", *((long long*) (t_pnt-16)) == 0 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (t_pnt-8)) == 100 ? "CORRETO!" : "INCORRETO!");
 
-    printf("==>> DESALOCANDO TUDO:\n");
-    memory_free(alloc_pointers[1]);
-    memory_free(alloc_pointers[2]);
-    printf("\tIND. DE USO: %s\n", *((long long *)(alloc_pointers[1] - 16)) == 0 ? "CORRETO!" : "INCORRETO!");
-    printf("\tTAMANHO: %s\n", *((long long *)(alloc_pointers[1] - 8)) == 50 ? "CORRETO!" : "INCORRETO!");
-    printf("\tIND. DE USO: %s\n", *((long long *)(alloc_pointers[2] - 16)) == 0 ? "CORRETO!" : "INCORRETO!");
-    printf("\tTAMANHO: %s\n", *((long long *)(alloc_pointers[2] - 8)) == 100 ? "CORRETO!" : "INCORRETO!");
 
-    printf("==>> DESALOCANDO A PILHA (ILEGAL):\n");
-    unsigned int alloc_return = memory_free((void *)alloc_pointers);
-    if (!alloc_return)
-        printf("\tO RETORNO DA LIBERAÇÃO FOI NULL!\n");
+	printf("==>> DESALOCANDO A PILHA (ILEGAL):\n");
+	unsigned long long stack_var = 0;
+	unsigned int alloc_return = memory_free((void*) &stack_var);
+	if (!alloc_return) printf("\tO RETORNO DA LIBERAÇÃO FOI NULL!\n");
 
-    return 0;
+	dismiss_brk();
+	*/
+
+	return 0;
+	
 }
